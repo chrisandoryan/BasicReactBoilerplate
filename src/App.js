1c3c5f0cdf2674fe9e1483ff2e033da5078faf1f
naufalprakoso <naufalprakoso24@gmail.com>
2020-10-11 00:34:34 +0700

    Update Emoticon UI

--- src/App.js
+++ src/App.js
@@ -12,18 +12,20 @@ function App() {
         <div>
             <iframe className={"game-frame"} src="https://gather.town/app/J5KcIR6M7dQr6YYH/BlankPink" frameBorder="0"/>
             <Row className={"fixed-bottom"}>
-                <Col md={3} />
-                <Col md={4}>
+                <Col md={4} />
+                <Col md={3}>
                     <Card
-                        className={"shadow rounded p-4"}
-                        style={{border: 0}}
+                        className={"shadow-lg rounded p-4"}
+                        style={{border: 0, backgroundImage: "linear-gradient(to right, #8400E2 0%, #bf4dcd 100%)"}}
                     >
-                        <h4 className={"text-center"}>Give your reaction</h4>
-                        <div className={"text-center"}>
-                            <RiEmotionSadLine style={{fontSize: 40}} className={"text-danger"} />
-                            <RiEmotionNormalLine style={{fontSize: 40}} className={"text-muted"} />
-                            <RiEmotionLine style={{fontSize: 40}} className={"text-success"}/>
-                        </div>
+                        <h4 className={"text-center text-white"}>Give your reaction</h4>
+                        <Card className={"shadow-lg mt-2"} style={{borderRadius: 25, border: 0}}>
+                            <div className={"text-center p-1"}>
+                                <RiEmotionSadLine style={{fontSize: 40}} className={"text-danger"} />
+                                <RiEmotionNormalLine style={{fontSize: 40}} className={"text-muted"} />
+                                <RiEmotionLine style={{fontSize: 40}} className={"text-success"}/>
+                            </div>
+                        </Card>
                     </Card>
                 </Col>
                 <Col md={5} />
