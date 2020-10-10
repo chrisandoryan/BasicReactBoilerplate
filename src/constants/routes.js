94d627e3701da96b0af8a9c15c283e0d3e49f2da
Steven Ferdianto <39341612+KentangRebus@users.noreply.github.com>
2020-10-10 15:23:48 +0700

    initial

---
+++ src/constants/routes.js
@@ -0,0 +1,24 @@
+const routes = [
+  {
+    name: "Login",
+    path: "/login",
+    component: "",
+  },
+  {
+    name: "Register",
+    path: "/register",
+    component: "",
+  },
+  {
+    name: "Home",
+    path: "/",
+    component: "",
+  },
+  {
+    name: 'Concert'
+    path: "/concert/:id",
+    component: "",
+  },
+];
+
+export default routes;
