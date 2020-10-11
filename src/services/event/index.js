830a236d9e40a5110057e2fb2e6585c4d39f8080
Siahaan <chrisando.siahaan@gmail.com>
2020-10-11 19:26:01 +0700

    added reaction button functionality

--- src/services/event/index.js
+++ src/services/event/index.js
@@ -5,6 +5,22 @@ import User from "../../models/user";
 const firebase = new Firebase();
 const db = firebase.db;
 
+export const sendReaction = (emot_id, event_id) => {
+    return db.collection(Event.collection)
+        .doc(event_id)
+        .collection("reactions")
+        .add({
+            emoticon: emot_id,
+            react_time: new Date()
+        });
+}
+
+export const checkReactionDone = (event_id, user_id) => {
+    db.collection(Event.collection)
+        .doc(event_id)
+        .collection("reactions")
+}
+
 export const getEventData = (event_id) => {
     return db.collection(Event.collection)
         .doc(event_id)
